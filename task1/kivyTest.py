1980c88892a0d8d4bbc65ad37759db3ccbf3a633
Adel elmala <adel.elmala2025@gmail.com>
2020-10-25 21:22:43 +0200

    added a label to the app

--- task1/kivyTest.py
+++ task1/kivyTest.py
@@ -1,35 +1,37 @@
-# import kivy
-# kivy.require('1.0.6') # replace with your current kivy version !
-
-# from kivy.app import App
-# from kivy.uix.label import Label
-
-
-# class MyApp(App):
-
-#     def build(self):
-#         return Label(text='Hello world')
-
-
-# if __name__ == '__main__':
-#     MyApp().run()
-
-# ================================
 
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-import matplotlib.pyplot as plt
 
-plt.plot([1, 23, 2, 4])
+from kivy.uix.boxlayout import BoxLayout
+# from kivy.uix.floatlayout import FloatLayout
+# from kivy.uix.anchorlayout import AnchorLayout
+
+from kivy.uix.label import Label
+
+import matplotlib.pyplot as plt
+import  numpy as np
+
+# plt.plot([1, 23,8, 2, 4])
+x = np.arange(0, 5, 0.1)  
+y = np.sin(x)  
+plt.plot(x, y)
 plt.ylabel('some numbers')
 
 class MyApp(App):
 
     def build(self):
-        box = BoxLayout()
-        box.add_widget(FigureCanvasKivyAgg(plt.gcf()))
-        return box
+        layout = BoxLayout()
+        # layout = FloatLayout()
+        # layout = AnchorLayout(anchor_x='right', anchor_y='bottom')
+        
+        # Could get rid off the label and add a legend to the plot that can be dynamically updated // Too lazy to do it now 
+        label = Label(text='Sensor Reading',pos=(50, 50))
+        plot = FigureCanvasKivyAgg(plt.gcf())
+        
+        layout.add_widget(label)
+        layout.add_widget(plot)
+        
+        return layout
 
 if __name__ == '__main__':
     MyApp().run()
